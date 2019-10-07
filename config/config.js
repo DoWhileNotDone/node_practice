dadedf351d52b65350f9a15953eb80c4c9fb2fd7
vagrant <vagrant@vagrant.vm>
2019-10-07 18:36:24 +0000

    Express Tutorial - DB Drivers and ORM
     * Added Sequelize/DB
     * Created initial folder structure for DB/ORM

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "development": {
+    "username": "express-mvp-dbuser",
+    "password": "password",
+    "database": "express-mvp-db",
+    "port": 5432,
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
