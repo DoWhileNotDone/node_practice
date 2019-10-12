e93725038a43b0344c838b2965256a7119f6b45a
DavegTheMighty <dave@example.com>
2019-10-12 21:00:37 +0100

    Node Practice - Add Lead Link and details page

--- controllers/leads.js
+++ controllers/leads.js
@@ -1,11 +1,30 @@
 const models = require('../models');
 
-
 exports.get_leads = function(req, res, next) {
   models.Lead.findAll().then(function(leads) {
     res.render('leads', {
        leads: leads,
-       title: 'Leads' 
+       title: 'Leads'
      });
   });
+}
+
+exports.get_lead = function(req, res, next) {
+  models.Lead.findOne({
+      where: {
+        id: req.params.lead_id
+      }
+  }).then(function(lead) {
+    res.render('lead', {
+       lead: lead,
+       title: 'Selected Lead'
+     });
+  });
+
+  // models.Lead.findByPk(req.params.lead_id).then(function(lead) {
+  //   res.render('lead', {
+  //      lead: lead,
+  //      title: 'Selected Lead'
+  //    });
+  // });
 }
--- routes/leads.js
+++ routes/leads.js
@@ -5,4 +5,6 @@ let leads = require('../controllers/leads');
 /* GET leads listing. */
 router.get('/leads', leads.get_leads);
 
+router.get('/leads/:lead_id', leads.get_lead);
+
 module.exports = router;
